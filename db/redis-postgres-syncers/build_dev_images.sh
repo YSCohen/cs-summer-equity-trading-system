62d397b15049612d8ea96f0559164da1bf978f91
Yehuda Cohen <78832113+YSCohen@users.noreply.github.com>
2026-07-01 13:48:07 -0400

    build dev images for arm too

--- db/redis-postgres-syncers/build_dev_images.sh
+++ db/redis-postgres-syncers/build_dev_images.sh
@@ -1,10 +1,22 @@
 #!/usr/bin/env bash
 
 echo "building trade-writer image..."
-docker build --file Containerfile -t ghcr.io/sm26-industrial-software-dev/trade-writer:dev --build-arg BIN_NAME=trade-writer --push .
+docker build --file Containerfile \
+    --platform linux/amd64,linux/arm64 \
+    --build-arg BIN_NAME=trade-writer \
+    --tag ghcr.io/sm26-industrial-software-dev/trade-writer:dev \
+    --push .
 
 echo "building db-syncer image..."
-docker build --file Containerfile -t ghcr.io/sm26-industrial-software-dev/db-syncer:dev --build-arg BIN_NAME=db-syncer --push .
+docker build --file Containerfile \
+    --platform linux/amd64,linux/arm64 \
+    --build-arg BIN_NAME=db-syncer \
+    --tag ghcr.io/sm26-industrial-software-dev/db-syncer:dev \
+    --push .
 
 echo "building price-cacher image..."
-docker build --file Containerfile -t ghcr.io/sm26-industrial-software-dev/price-cacher:dev --build-arg BIN_NAME=price-cacher --push .
+docker build --file Containerfile \
+    --platform linux/amd64,linux/arm64 \
+    --build-arg BIN_NAME=price-cacher \
+    --tag ghcr.io/sm26-industrial-software-dev/price-cacher:dev \
+    --push .
